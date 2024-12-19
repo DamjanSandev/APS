071ca39de420656034f7f3bf9ec629a005fcbf34
dsand <frank.nords@protonmail.com>
2024-12-19 23:55:04 +0100

    Commit

---
+++ src/Kolokvium/K2/Lab_8_Doma_2024.java
@@ -0,0 +1,48 @@
+package Kolokvium.K2;
+
+import java.util.Scanner;
+
+public class Lab_8_Doma_2024 {
+    public static int depth(BNode<Integer> node, int element) {
+
+        if (element == node.info) {
+            return 1;
+        }
+        if (element < node.info) {
+            return 1 + depth(node.left, element);
+        } else {
+            return 1 + depth(node.right, element);
+        }
+    }
+
+    public static int depthNonRecursive(BNode<Integer> node, int element) {
+        int counter = 1;
+        while (node.info != element) {
+            if (element < node.info) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+            counter++;
+        }
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), q = sc.nextInt();
+        sc.nextLine();
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        for (int i = 0; i < n + q; i++) {
+            String[] parts = sc.nextLine().split("\\s+");
+            if (parts[0].equals("insert")) {
+                tree.insert(Integer.parseInt(parts[1]));
+            } else {
+                System.out.println(depthNonRecursive(tree.getRoot(), Integer.parseInt(parts[1])));
+            }
+
+        }
+
+    }
+
+}
---
+++ src/Kolokvium/K2/Lab_8_Vo_Lab_2024.java
@@ -0,0 +1,30 @@
+package Kolokvium.K2;
+
+import java.util.Scanner;
+
+public class Lab_8_Vo_Lab_2024 {
+    public static int smallerThanGivenNode(BNode<Integer> node) {
+        if (node == null) {
+            return 0;
+        }
+        return 1 + smallerThanGivenNode(node.left) + smallerThanGivenNode(node.right);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), q = sc.nextInt();
+        sc.nextLine();
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        for (int i = 0; i < n + q; i++) {
+            String[] parts = sc.nextLine().split("\\s+");
+            if (parts[0].equals("insert")) {
+                tree.insert(Integer.parseInt(parts[1]));
+            } else {
+                BNode<Integer> node = tree.find(Integer.parseInt(parts[1]));
+                System.out.println(smallerThanGivenNode(node.left));
+            }
+
+        }
+    }
+}
